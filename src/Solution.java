348e1ed9a8a26aaf75dfcb00eb5927bf9f979510
o_Qbert_o <ermakov.sascha2015yandex.ru@gmail.com>
2020-08-25 16:07:32 +0300

    Commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,7 @@
+import basicsOfSoftwareCodeDevelopment.cycles.task2.FunctionValue;
+
 public class Solution {
     public static void main(String[] args) {
-
+        FunctionValue.findTheValueOfTheFunctionOnTheSegment(-6, 15, 2);
     }
 }
